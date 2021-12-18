62aba3f4a850f73fe4240210f266ff9b9a91b2cc
Guilherme Augusto Rocha Sousa <a.guilhermerocha250@gmail.com>
2021-12-18 16:46:25 -0300

    Add files via upload

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import userRouter from '@modules/users/routes/user.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const router = Router()
 
@@ -10,5 +11,6 @@ router.use('/products', productsRouter);
 router.use('/users', userRouter);
 router.use('/session', sessionsRouter);
 router.use('/password', passwordRouter);
+router.use('/profile', profileRouter);
 
 export default router;
